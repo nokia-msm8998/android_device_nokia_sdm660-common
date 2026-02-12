c613cad8050b8bdf92e2128c783c28c17b2ba6b7
Michael Bestas <mkbestas@lineageos.org>
2026-02-12 19:23:07 +0000

    sdm660-common: Compile the kernel without GCC
    Change-Id: I882c0d8600f88c1bb939c7ee247230ec5e70338a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -58,6 +58,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CONFIG := vendor/sdm660-perf_defconfig vendor/nokia/nokia.config
+TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/nokia/sdm660
 
 # Media
